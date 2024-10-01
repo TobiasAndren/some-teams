45cdecd9fb7c18928a84634796b85c07ce89801c
TobiasAndren <tobiasandren@hotmail.com>
2024-10-01 15:47:54 +0200

    added some content to index.php

---
+++ Project/index.php
@@ -0,0 +1,15 @@
+<?php
+
+include __DIR__ . "/teams.php";
+include __DIR__ . "/header.php";
+?>
+
+<header>
+    <h1>Some-teams</h1>
+</header>
+<nav>
+    <ul>
+        <li><a href="index.php">Start</a></li>
+        <li><a href="about.php">About</a></li>
+    </ul>
+</nav>
