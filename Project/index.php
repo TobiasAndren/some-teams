634b8d7dc0d3bda358ac1b393d598e6ba72a5ceb
TobiasAndren <tobiasandren@hotmail.com>
2024-10-01 16:10:01 +0200

    added about.php and also added some content to index.php

--- Project/index.php
+++ Project/index.php
@@ -2,3 +2,13 @@
 
 include __DIR__ . "/teams.php";
 include __DIR__ . "/header.php";
+
+foreach ($teams as $team) { ?>
+
+    <article>
+        <img src="<?= $team['logo'] ?>">
+        <h2>League: <?= $team['league'] ?></h2>
+        <p>Uefa coefficeient Ranking: <?= $team['uefa-coefficient-ranking'] ?></p>
+    </article>
+
+<?php }
