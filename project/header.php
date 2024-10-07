04676e69a094762b6d941248f6b0746d61c7767c
TobiasAndren <tobiasandren@hotmail.com>
2024-10-07 10:12:39 +0200

    changed the title

--- project/header.php
+++ project/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Some-teams</title>
+    <title>Some Teams</title>
     <style>
         body {
             background-color: snow;
